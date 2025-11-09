ef40340a55093eccde890963e89ec54bf18a554b
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2025-11-09 18:18:33 +0800

    Time: 0 ms (100%), Space: 7.8 MB (71.74%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        times = 0;
-        op(num1,num2);
-        return times;
+        return times(num1,num2);
     }
 private:
-    int times;
-    void op(int x,int y){
-        if(x==0||y==0)return;
+    int times(int x,int y){
+        if(x==0||y==0)return 0;
         if(x<y)swap(x,y);
-        times += x/y;
-        op(y,x%y);
+        return x/y+times(y,x%y);
     }
 };
