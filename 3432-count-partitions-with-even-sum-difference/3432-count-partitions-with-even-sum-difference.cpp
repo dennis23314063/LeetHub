09dd4a45ae5a25b412b4d749e7c7e8705c9ec880
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2025-12-05 09:46:08 +0800

    Time: 0 ms (100%), Space: 22.5 MB (53.79%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -2,16 +2,10 @@ class Solution {
 public:
     int countPartitions(vector<int>& nums) {
         int sum =0;
-        int acc = 0;
-        int ans = 0;
         for(auto &i:nums){
             sum+=i;
-            // if(sum%2==0)ans++;
         }
-        for(auto &i:nums){
-            acc+=i;
-            ans+=(sum-2*acc)%2==0;
-        }
-        return max(0,ans-1);
+        if(sum%2==0)return nums.size()-1;
+        return 0;
     }
 };
