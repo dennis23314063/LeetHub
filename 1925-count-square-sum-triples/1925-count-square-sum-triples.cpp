1ca25aa670d6ccad553867acf29646dc4b8900e2
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2025-12-08 13:23:48 +0800

    Time: 0 ms (100%), Space: 7.9 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int countTriples(int n) {
-        //3 2
         int ans=0;
         for(int i=2;i*i<=n;i++){
-            for(int j=1;j<i;j++){
-                if((i&1)==(j&1))continue;
+            for(int j=1+(i&1);j<i;j+=2){
                 if(gcd(i,j)==1)ans+=n/(i*i+j*j)*2;
             }
         }
