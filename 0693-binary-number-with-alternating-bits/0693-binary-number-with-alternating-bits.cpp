a02b407864559df176e8e5717781d5a58285d97c
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2026-02-18 19:00:07 +0800

    Time: 0 ms (100%), Space: 7.9 MB (30.66%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int count;
-        int low = n&(-n);
-        if(n%2)count=0;
-        else if(low==2)count=1;
-        else return false;
-        while(n){
-            if(low!=(1<<count))return false;
-            n-=low;
-            count+=2;
-            low = n&(-n);
-        }
-        return true;
-
+        unsigned int x = n^(n>>1);
+        cout<<x<<endl;
+        return (x&(x+1))==0;
     }
 };
