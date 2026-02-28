053b9925363f9237c428913e0c230de2bfc98cbc
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2026-02-28 14:03:14 +0800

    Time: 28 ms (71.14%), Space: 8 MB (89.26%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        long long ans=0;
+        int mod = 1e9+7;
+        int p=0;
+        for(int i=1;i<=n;i++){
+            if((i&(i-1))==0)p++;
+            ans<<=p;
+            ans|=i;
+            ans%=mod;
+        }
+        return ans;
+    }
+};
