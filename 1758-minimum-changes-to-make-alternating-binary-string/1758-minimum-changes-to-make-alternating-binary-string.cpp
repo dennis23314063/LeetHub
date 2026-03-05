f7ac6a65632a162da04e66fdf32008babf5015be
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2026-03-05 20:30:33 +0800

    Time: 0 ms (100%), Space: 9.6 MB (20.96%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int ans=0;
+        bool check = true;
+        for(auto i:s){
+            if(check&&i=='0'||!check&&i=='1')ans++;
+            check = !check;
+        }
+        cout<<ans<<' '<<s.size()-ans<<endl;
+        return std::min(ans,(int)s.size()-ans);
+    }
+};
