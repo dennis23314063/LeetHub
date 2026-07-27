7613198ec0e5d5dadebe4cfcd0ca87bbc7496001
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2026-07-27 12:18:51 +0800

    Time: 0 ms (100%), Space: 13.3 MB (85.48%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n= nums.size();
+        int a=0;
+        int b=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>a){
+                b=a;
+                a = nums[i];
+            }
+            else if(nums[i]>b){
+                b = nums[i];
+            }
+        }
+        return (a-1)*(b-1);
+    }
+};
