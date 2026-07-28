4a6a832778400c6caa53cbe1cae8238cc4f23a90
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2026-07-29 00:08:20 +0800

    Time: 153 ms (15.6%), Space: 53.9 MB (99.12%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        vector<int> v(26,0);
-        for(auto &i:s)v[i-'a']++;
-        string ans=s;
-        int n = ans.size();
-        for(int i=0,j=0;i<26;i++){
-            if(v[i]%2)ans[n/2]=(char)(i+'a');
-            v[i]/=2;
-            while(v[i]--)ans[j] = ans[n-j-1] = (char)(i+'a'),j++;
-        }
-        return ans;
+        
+        int n =s.size();
+        sort(s.begin(),s.begin()+n/2);
+        sort(s.begin()+n/2+n%2,s.end(),greater<char>());
+        return s;
     }
 };
