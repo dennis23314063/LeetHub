f3b10ac219060f96df688dbcade3f5d8c2c1c35a
趙邦鈞 <82079905+dennis23314063@users.noreply.github.com>
2026-09-20 21:19:45 +0800

    Time: 4 ms (9.52%), Space: 9.6 MB (78.4%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n = s.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=(26-(s[i]-'a'))*(i+1);
+        }
+        return ans;
+    }
+};
